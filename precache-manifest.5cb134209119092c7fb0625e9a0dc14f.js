13bbc0c86479ceb50c254ad877c82f9f3fa7e2f8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-13 19:30:01 +0000

    Deploy eersinyildizz/ConnectApp to github.com/eersinyildizz/ConnectApp.git:gh-pages

---
+++ precache-manifest.5cb134209119092c7fb0625e9a0dc14f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b488df7d70a25c91a27f",
+    "url": "/ConnectApp/static/css/main.b351d588.chunk.css"
+  },
+  {
+    "revision": "b488df7d70a25c91a27f",
+    "url": "/ConnectApp/static/js/main.b488df7d.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/ConnectApp/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "9b6febc1bb2d016f28c3",
+    "url": "/ConnectApp/static/js/runtime~main.9b6febc1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ConnectApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d2693ac3e4f3ca119e9b7fbade67c294",
+    "url": "/ConnectApp/index.html"
+  }
+];
